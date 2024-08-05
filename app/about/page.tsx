c7243cf18a3c30d351870108362f36955cbe28ab
Tawan Reis <tawanreis1@hotmail.com>
2024-08-05 00:30:52 -0300

    chore: add shadcn setup

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import Image from "next/image";
+import { useState } from "react";
+
+const About = () => {
+
+  const [] = useState();
+  return <h1 className="text-green-500">about green text</h1>
+}
+
+export default About;
