47a3fa38921a89e0cb40723d50eabfcf49ba8f18
Tawan Reis <tawanreis1@hotmail.com>
2024-08-05 00:43:39 -0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import { Button } from "./_components/ui/button";
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
   return <Button>teste</Button>
 }
 
-export default Home;
+export default Home
