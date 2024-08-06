6a5ad59de3fa14db7a4d70682618e434a7cef08e
Tawan Reis <tawanreis1@hotmail.com>
2024-08-05 23:03:35 -0300

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,103 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import Image from "next/image"
 import { Button } from "./_components/ui/button"
+import { Card, CardContent } from "./_components/ui/card"
+import { Input } from "./_components/ui/input"
+import { db } from "./_lib/prisma"
+import { quickSearchOptions } from "./_constants/quicksearch"
+import BookingItem from "./_components/booking-item"
+import BarbershopItem from "./_components/barbershop-item"
 
-const Home = () => {
-  return <Button>teste</Button>
+const Home = async () => {
+  const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc",
+    },
+  })
+
+  return (
+    <div>
+      {/* HEADER */}
+      <Header />
+
+      <div className="p-5">
+        {/* TEXTO */}
+        <h2 className="text-xl font-bold">Olá, Tawan!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        {/* BUSCA */}
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        {/* BUSCA RÁPIDA*/}
+        <div className="mt-6 flex gap-3 overflow-x-scroll [&::-webkit-scrollbar]:hidden">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image
+                src={option.imageUrl}
+                width={16}
+                height={16}
+                alt={option.title}
+              />
+              {option.title}
+            </Button>
+          ))}
+        </div>
+
+        {/* IMAGEM */}
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com FSW Barber"
+            src="/banner-01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+
+        {/* AGENDAMENTO */}
+        <BookingItem />
+
+        {/* RECOMENDADOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Recomendados
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((barbershop) => (
+            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+          ))}
+        </div>
+
+        {/* RECOMENDADOS */}
+        <h2 className="mb-3 mt-6 text-xs font-bold uppercase text-gray-400">
+          Populares
+        </h2>
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {popularBarbershops.map((popularBarbershops) => (
+            <BarbershopItem
+              key={popularBarbershops.id}
+              barbershop={popularBarbershops}
+            />
+          ))}
+        </div>
+
+        <footer>
+          <Card>
+            <CardContent className="px-5 py-6">
+              <p className="text-sm text-gray-400">
+                © 2023 Copyright <span className="font-bold">FSW Barber</span>
+              </p>
+            </CardContent>
+          </Card>
+        </footer>
+      </div>
+    </div>
+  )
 }
 
 export default Home
